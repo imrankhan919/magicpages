a398476d5312dcf36580b57e37d28940e521e7a3
imrankhan919 <imrankhan919.ik@gmail.com>
2026-05-26 19:03:46 +0530

    credits middleware added

--- server/routes/imageGenRoutes.js
+++ server/routes/imageGenRoutes.js
@@ -1,11 +1,12 @@
 import express from "express"
 import protect from "../middlewares/authMiddleware.js"
 import imageController from "../controllers/imageGenController.js"
+import checkAndUpdateCredits from "../middlewares/creditUpdateMiddleware.js"
 
 
 const router = express.Router()
 
-router.post("/generate", protect.forUser, imageController.transformImage)
+router.post("/generate", protect.forUser, checkAndUpdateCredits, imageController.transformImage)
 
 
 export default router
